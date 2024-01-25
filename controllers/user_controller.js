a43d3b1824c31df8b6c8893105fa9f65e9646d32
Jian Jang <pureunnabi@gmail.com>
2024-01-25 14:11:06 +0900

    카드 뉴스 저장 컨트롤러 수정

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -398,8 +398,8 @@ exports.saveCardNews = (req, res) => {
     }
 
     // 2. 카드뉴스 저장
-    const saveCardNewsQuery = 'INSERT INTO cardNews (title, plcae, open_time, close_time, price, image_url, createDate, userid) VALUES (?, ?, ?, ?)';
-    const cardNewsValues = [title, content, createDate, userid];
+    const saveCardNewsQuery = 'INSERT INTO cardNews (title, plcae, open_time, close_time, price, image_url, createDate, userid) VALUES (?, ?, ?, ?,?, ?, ?, ?)';
+    const cardNewsValues = [title, plcae, open_time, close_time, price, image_url, createDate, userid];
 
     connection.query(saveCardNewsQuery, cardNewsValues, (cardNewsErr, cardNewsResult) => {
       if (cardNewsErr) {
