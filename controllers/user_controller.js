8cffb255fa3e984feed8cb5c1a084a13d2b80cda
Jian Jang <pureunnabi@gmail.com>
2024-06-17 14:49:19 +0900

    카드뉴스 쿼리 수정

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -506,7 +506,7 @@ exports.saveCardNews = (req, res) => {
       return res.status(500).json({ error: 'Server error', details: err.message });
     }
 
-    const { place, open_time, close_time, price, content, start, userid } = req.body;
+    const {title, place, open_time, close_time, price, userid } = req.body;
     const image_url = req.file ? `/uploads/${req.file.filename}` : null; // Uploaded image URL
 
     // Insert card news into database
